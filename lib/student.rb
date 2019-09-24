185c9a8b87739e0856f997eceb8dfc184e795084
github username <chyrellelewis@gmail.com>
2019-09-24 05:52:12 -0500

    restore file 6

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,75 @@
 require_relative "../config/environment.rb"
 
-class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  class Student
+    attr_accessor :name, :grade 
+    attr_reader :id
 
+    def initialize(id=nil, name, grade)
+        @id = id
+        @name = name
+        @grade = grade
+    end
 
+    def self.create_table
+      sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS
+      students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      )
+      DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+      sql = <<-SQL
+      DROP TABLE students
+      SQL
+      DB[:conn].execute(sql)
+    end
+
+    def save
+    sql = <<-SQL
+    INSERT INTO students (name, grade, id)
+    VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
 end
+
+    def self.create(name:, grade:)
+      student = Student.new(name, grade)
+      student.save
+      student
+    end
+  
+
+    def self.new_from_db
+      
+      new_student = Student.new
+      new_student.id = row[0]
+      new_student.name = row[1]
+      new_student.grade = row[2]
+      new_student
+    end
+    
+
+    def self.find_by_name(name)
+      # find the student in the database given a name
+      # return a new instance of the Student class
+      sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+      SQL
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+    end.first
+  end
+  
+ 
+
+
